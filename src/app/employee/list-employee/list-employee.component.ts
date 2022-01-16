a9c5abe92eeb37d83ec01d03579ac58dd083aef2
PratikSonawaneAvercast <62496920+PratikSonawaneAvercast@users.noreply.github.com>
2022-01-17 00:59:01 +0530

    commit with login logout

--- src/app/employee/list-employee/list-employee.component.ts
+++ src/app/employee/list-employee/list-employee.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild,Inject} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { EmployeeService } from 'src/app/services/employee.service';
+import { DialogService } from 'src/app/shared/dialog.service';
 
 export interface Employee {
   name: string;
@@ -20,17 +22,34 @@ const ELEMENT_DATA: Employee[] = [];
 export class ListEmployeeComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'name', 'username', 'email', 'actions'];
-  dataSource = ELEMENT_DATA;
+  dataSource: MatTableDataSource<any> = new MatTableDataSource()
+  dataSource1 = ELEMENT_DATA;
+
+  //(keyup)="applyFilter($event.target.value)" 
 
   //type casting
   listEmployee: Employee[] = [];
 
-  constructor(private employeeService:EmployeeService) { }
+  constructor(private employeeService:EmployeeService,
+    private dialogService : DialogService) { }
 
   ngOnInit(): void {
-      this.employeeService.listEmployee().subscribe(data=>{
-      this.dataSource = data;
-      });
+      this.employeeService.listEmployee().subscribe((data:any)=>{
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource1 = data;
+      });      
   }
 
+  applyFilter(value: string) {
+    document.getElementById('xyz');
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+    const filterValue = value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  clickMethod(){
+    this.dialogService.openConfirmDialog();
+  }
+
+  
 }
